58f24a068b9941b1f4e534a9d72ddcaed4c7f424
Mac User <macuser@Macs-MacBook-Pro.local>
2023-02-14 10:04:18 +0200

    project init

---
+++ Github_search/Github_search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Github_search
+//
+//  Created by Mac User on 14.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
